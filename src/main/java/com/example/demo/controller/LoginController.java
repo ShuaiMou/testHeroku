fa33ef6c16d4e5761f036857f1d8ae3563561dc4
ShuaiMou <shuaimou77@gmail.com>
2020-08-29 14:34:29 +1000

    test ResponseEntity

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,18 +1,28 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Auther: Saul
  * @Date: 24/8/20 10:52
  * @Description:
  */
-@RestController
+@Controller
 public class LoginController {
 
     @GetMapping("/login")
+    @ResponseBody
     public String login(String name){
         return name + " login successfully";
     }
+
+    @GetMapping("/hello")
+    ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Hello World!", HttpStatus.BAD_REQUEST);
+    }
+
 }
